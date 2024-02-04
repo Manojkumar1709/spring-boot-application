4fd1846f11abf84994d1a1d04082226eaac7da1a
Manojkumar1709 <mm2040922@gmail.com>
2024-02-04 19:46:58 +0530

    Spring Boot Application

---
+++ src/main/java/com/example/java/helloworld/HelloworldApplication.java
@@ -0,0 +1,21 @@
+package com.example.java.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	@RequestMapping("/")
+    String helloWorld() {
+        return "<h3>Hello Welcome to Spring Boot Application<h3>";
+    }
+
+}
