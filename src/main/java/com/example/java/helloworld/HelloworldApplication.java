ffff1dc5b7df529fbade20d1d9fedcd94a7512b7
Manojkumar1709 <mm2040922@gmail.com>
2024-02-08 23:34:27 +0530

    Updated Main file

--- src/main/java/com/example/java/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/java/helloworld/HelloworldApplication.java
@@ -2,8 +2,8 @@ package com.example.java.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @SpringBootApplication
@@ -13,7 +13,7 @@ public class HelloworldApplication {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
-	@GetMapping("/")
+	@RequestMapping("/")
     String helloWorld() {
         return "Welcome to Spring Boot Application";
     }
